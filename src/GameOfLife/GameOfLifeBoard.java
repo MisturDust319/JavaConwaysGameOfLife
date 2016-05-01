0c883a7faec2ab983fa4e2263faa8b95de032b4e
Stan S <jarlofearls@gmail.com>
2016-05-01 19:48:21 -0400

    Modified the neighbor detection function

--- src/GameOfLife/GameOfLifeBoard.java
+++ src/GameOfLife/GameOfLifeBoard.java
@@ -1,5 +1,7 @@
 package GameOfLife;
 
+import java.lang.reflect.Array;
+
 import board.Board;
 
 public class GameOfLifeBoard extends Board<LifeNode> {
@@ -8,11 +10,66 @@ public class GameOfLifeBoard extends Board<LifeNode> {
 		super();
 		
 		//find the neighbors for each node in the board
-		for(int i = 0; i < this.getLength(); i++) {
-			for(int j = 0; i < this.getWidth(); j++) {
-				if( i == 0 ) {
-					this.board[i][j].addNeighbor(this.board[i+1][j]);
+		for(int y = 0; y < this.getLength(); y++) {
+			for(int x = 0; y < this.getWidth(); x++) {
+				//this SHOULD BE REWRyTTEN TO ACCOMODATE SPECifyC CASES
+				// OTHERWySE, THE MOST GENERyC CASE (A FULLY SURROUNDED NODE)
+				// WyLL BE VERY HARD TO DESCRyBE
+				
+				//create a ref to the current node of the board 
+				LifeNode node = this.board[x][y];
+				//int rightBorder = this.getWidth() - 1;
+				//int bottomBorder = this.getLength() - 1;
+				
+				//create an array of the coordinates of the
+				//	neighboring nodes
+				int[][] neighborCoordindates = { {x - 1, y - 1},
+											  	 {x,	 y - 1},
+											   	 {x + 1, y - 1},
+											   	 {x - 1, y},
+											   	 {x + 1, y},
+											   	 {x - 1, y + 1},
+											   	 {x,	 y + 1},
+											   	 {x + 1, y + 1} };
+				
+				for( int i = 0; i < 8; i++ ) {
+					try {
+						node.addNeighbor(this.board[neighborCoordindates[i][0]][neighborCoordindates[i][1]]);
+					}
+					catch(ArrayIndexOutOfBoundsException outOfBounds) {
+						//do nothing, as we are using the catch
+						//	block to allow the neighbor search to skip
+						//	those areas out of bounds
+					}
 				}
+				
+				/*
+				//if we are at the top border:
+				if( y == 0 ) {
+					//if we are currently in the upper left corner:
+					if( x == 0 ) {
+						node.addNeighbor(new LifeNode[]{ this.board[0][1],
+												  	this.board[1][0],
+												  	this.board[1][1]});
+					}
+					//if we are currently in the upper right corner:
+					else if ( x == rightBorder) {
+						node.addNeighbor(new LifeNode[]{ this.board[rightBorder - 1][0],
+														this.board[rightBorder - 1][1],
+														this.board[rightBorder][1] });
+					}
+					//otherwise, we just border the top edge
+					else {
+						node.addNeighbor(new LifeNode[]{ this.board[x - 1][0],
+													this.board[x - 1][1],
+													this.board[x][1],
+													this.board[x + 1][1],
+													this.board[x + 1][0] });
+					}
+				}				
+				else if( x == 0 ) {
+					//w
+				}*/
 			}
 		}
 	}
--- src/GameOfLife/LifeNode.java
+++ src/GameOfLife/LifeNode.java
@@ -1,6 +1,7 @@
 package GameOfLife;
 
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Set;
 
 //a class for a node in Conway's Game of Life 
@@ -28,22 +29,44 @@ public class LifeNode {
 	//sets the state of the LifeNode to
 	//	the proper state based on the
 	//	state of its neighbors
-	private void setStatus() {
+	/*
+	 * THIS NEEDS TO BE REWRITTEN!
+	 * THIS ONLY CHECKS FOR NEIGHBOR COUNT
+	 * IT NEEDS TO FIND THE NUMBER OF LIVING
+	 * NEIGHBORS! IT NEEDS TO CHECK THE NEIGHBORS
+	 * LIFE STATUS AND SUM THAT UP
+	 */
+	public void setStatus() {
 		//check the size of neighbors set
-		int size = neighbors.size();
+		//int size = neighbors.size();
 		
-		/* The rules for the survival of a cell during Conway's Game of Life
-		 * From Wikipedia:
+		//create an iterator for the neighbors set
+		Iterator<LifeNode> itr = neighbors.iterator();
+		//create an int for recording the number of
+		//live neighboring nodes
+		int sum = 0;
+		while(itr.hasNext() && sum < 4) {
+			//get the next neighbor
+			LifeNode elem = itr.next();
+			//check if this neighbor is alive
+			if( elem.checkLife() ) {
+				//if so, increment sum
+				sum++;
+			}
+		}
+		
+		/*Now to check whether the node survives, dies, or is born
+		 *  The rules for the survival of a cell during Conway's Game of Life from Wikipedia:
 		 * 	1. Any live cell with fewer than two live neighbours dies, as if caused by under-population.
 		 * 	2. Any live cell with two or three live neighbours lives on to the next generation.
 		 * 	3. Any live cell with more than three live neighbours dies, as if by over-population.
 		 * 	4. Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
 		 * */
-		if( size < 2 )
+		if( sum < 2 )
 			life = false;
-		else if( size >= 2 && size <= 3 && (life == true))
+		else if( sum >= 2 && sum <= 3 && (life == true))
 			life = true;
-		else if( size > 3)
+		else if( sum > 3)
 			life = false;
 		else
 			life = true;
@@ -60,6 +83,9 @@ public class LifeNode {
 			addNeighbor(nodes[i]);
 		}
 	}
+	
+	//return the node's life status
+	public boolean checkLife() { return life; }
 	
 	//a bool to flag whether the node
 	//	is alive or dead
