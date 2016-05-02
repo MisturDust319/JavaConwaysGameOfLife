2f65f631c4522903200b15588c26de8db19ead6d
Stan S <jarlofearls@gmail.com>
2016-05-01 22:14:54 -0400

    Started testing program

--- src/GameOfLife/GameOfLife.java
+++ src/GameOfLife/GameOfLife.java
@@ -1,11 +1,14 @@
 
 package GameOfLife;
 
+import GameOfLife.GameOfLifeBoard;
+
 public class GameOfLife {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		GameOfLifeBoard board = new GameOfLifeBoard();
+		board.draw();
 	}
 
 }
